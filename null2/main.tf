a3144a3785096e2599dcb6297591c7a4ed357b98
Igor Dulger <igordulger@gmail.com>
2021-05-21 15:28:47 +0300

    add more dirs

---
+++ null2/main.tf
@@ -0,0 +1,13 @@
+resource "null_resource" "resource1" {
+  provisioner "local-exec" {
+    command = "echo $ENV"
+    environment = {
+      ENV = "Hello big world!"
+    }
+  }
+  
+  
+  triggers = {
+    test = format("example+%s", timestamp())
+  }
+}
