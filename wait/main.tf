c372bae891209ef07aa7cc2923f0e0f9523864ac
igor-dulger <igordulger@gmail.com>
2022-01-12 14:54:46 +0200

    Update main.tf

--- wait/main.tf
+++ wait/main.tf
@@ -13,7 +13,7 @@ resource "random_integer" "timeout" {
 
 resource "null_resource" "wait" {
   triggers = {
-    run_id = var.run_id
+    test = format("example+%s", timestamp())
   }
   provisioner "local-exec" {
     command = "sleep 210"
